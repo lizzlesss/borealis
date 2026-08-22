9ff2b0d100a6456f648d4065e2a4f6413ef2d68b
casey / liz <63914304+lizzlesss@users.noreply.github.com>
2026-08-22 15:13:19 -0400

    replace power-profiles-daemon with tuned, add nbfc-linux

--- build_files/packages/pkgs.sh
+++ build_files/packages/pkgs.sh
@@ -2,4 +2,8 @@
 
 set -eoux pipefail
 
-pacman -Sy --noconfirm intel-lpmd
+pacman -R --noconfirm power-profiles-daemon
+pacman -Sy --noconfirm intel-lpmd tuned tuned-ppd curl wget
+
+wget https://github.com/nbfc-linux/nbfc-linux/releases/download/0.5.3/arch-linux-nbfc-linux-git-0.5.3-1-x86_64.pkg.tar.zst
+pacman -U --noconfirm ./arch-linux-nbfc-linux-git-0.5.3-1-x86_64.pkg.tar.zst
