ec941adf6641de858b0f9213625db80583752cbd
casey / liz <63914304+lizzlesss@users.noreply.github.com>
2026-08-22 15:19:41 -0400

    add alsa-firmware and sof-firmware

--- build_files/packages/pkgs.sh
+++ build_files/packages/pkgs.sh
@@ -3,7 +3,7 @@
 set -eoux pipefail
 
 pacman -R --noconfirm power-profiles-daemon
-pacman -Sy --noconfirm intel-lpmd tuned tuned-ppd curl wget
+pacman -Sy --noconfirm intel-lpmd tuned tuned-ppd curl wget alsa-firmware sof-firmware
 
 wget https://github.com/nbfc-linux/nbfc-linux/releases/download/0.5.3/arch-linux-nbfc-linux-git-0.5.3-1-x86_64.pkg.tar.zst
 pacman -U --noconfirm ./arch-linux-nbfc-linux-git-0.5.3-1-x86_64.pkg.tar.zst
