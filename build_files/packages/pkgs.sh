cf5170015fb53c980245dc9a6beb2bc9e820fcdf
casey / liz <63914304+lizzlesss@users.noreply.github.com>
2026-08-24 10:59:14 -0400

    add scx-scheds and scx-tools

--- build_files/packages/pkgs.sh
+++ build_files/packages/pkgs.sh
@@ -3,7 +3,14 @@
 set -eoux pipefail
 
 pacman -R --noconfirm power-profiles-daemon
-pacman -Sy --noconfirm intel-lpmd tuned tuned-ppd curl wget alsa-firmware sof-firmware
+pacman -Sy --noconfirm \
+    alsa-firmware \
+    intel-lpmd \
+    scx-scheds \
+    sof-firmware \
+    tuned \
+    tuned-ppd \
+    wget
 
 wget https://github.com/nbfc-linux/nbfc-linux/releases/download/0.5.3/arch-linux-nbfc-linux-git-0.5.3-1-x86_64.pkg.tar.zst
 pacman -U --noconfirm ./arch-linux-nbfc-linux-git-0.5.3-1-x86_64.pkg.tar.zst
